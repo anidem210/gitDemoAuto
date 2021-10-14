8232683c96e3f25ceb38bfa485fd2cede9f3830b
karl <karl.lohr.m@gmail.com>
2021-10-14 15:35:18 -0700

    holas 4 5 y 6

--- src/test/java/Cucumber/Automation/AppTest.java
+++ src/test/java/Cucumber/Automation/AppTest.java
@@ -20,5 +20,9 @@ public class AppTest
         System.out.println("hola2");
         System.out.println("hola3");
         
+        System.out.println("hola4");
+        System.out.println("hola5");
+        System.out.println("hola6");
+        
     }
 }
